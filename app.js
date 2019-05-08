c1076f1e90f2969ce9d1d2251bb6e148cd693601
Carlos Arias <Carlos_Arias@MALAGANAV.local>
2019-05-08 17:31:36 +0200

    primercommit

---
+++ app.js
@@ -0,0 +1,13 @@
+const fs=require('fs');
+
+let base=6;
+let data='';
+
+for (let i=1;i<=10;i++){
+    data+=`${base}*${i}=${base*i}\r\n`;
+    console.log('hola');
+}
+fs.writeFile(`tables/tablas-${base}.txt`,data,(err)=>{
+    if(err) throw err;
+    console.log(`El fichero tabla-${base}.txt ha sido creado`);
+})
